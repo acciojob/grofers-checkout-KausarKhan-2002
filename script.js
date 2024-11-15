5a2ad9c907976cab3a09a185fa2344bc7bd17107
Kausar Khan <156500913+KausarKhan-2002@users.noreply.github.com>
2024-11-15 10:38:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,6 +4,22 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
+  let table = document.querySelector("table");
+  let tr = document.createElement("tr");
+  let tdTotalPrice = document.createElement("td");
+  let tdShowTotalPrice = document.createElement("td");
+  let itemPrice = document.querySelectorAll(".price");
+
+  let totalPrice = 0;
+
+  itemPrice.forEach((price) => (totalPrice += Number(price.innerText)));
+
+  tdTotalPrice.innerHTML = "total price";
+  tdShowTotalPrice.innerHTML = totalPrice;
+  tr.appendChild(tdTotalPrice);
+  tr.appendChild(tdShowTotalPrice);
+
+  table.appendChild(tr);
   
 };
 
